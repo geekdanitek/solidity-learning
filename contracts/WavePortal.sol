a9b9fc6e9097271a3840071af3954b25ff51ca36
Daniel Adedeji <adedejiayokunledaniel@gmail.com>
2021-12-07 12:30:19 +0100

    initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor(){
+        // console.log("Hello, my first smart contract");
+        
+    }
+}
+
+// 333 325 371
